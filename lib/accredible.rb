22a708393f1ac3aa1e9ffae4c6c6fe093ed29d5f
Deepender Singla <deependersingla@Deependers-MacBook-Pro.local>
2015-03-09 10:23:46 +0530

    test added

--- lib/accredible.rb
+++ lib/accredible.rb
@@ -1,5 +1,17 @@
 require "accredible/version"
 
 module Accredible
-  # Your code goes here...
+  class << self
+  	attr_accessor :api_key
+  end
+
+  def self.request(method, url, params={}, headers={}, api_base_url=nil)
+  	unless api_key 
+      raise AuthenticationError.new('No API key provided. ' \
+        'Set your API key using "Accredible.api_key = <API-KEY>". ' \
+        'You can get API keys from the Accredible web interface. ' \
+        'See https://accredible.com for details, or email support@accredible.com ' \
+        'if you have any questions.')
+    end
+  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'pry'
+require 'accredible'
+
+#Rspec.configure do |config|
+ # config.formatter = nested
+#end
